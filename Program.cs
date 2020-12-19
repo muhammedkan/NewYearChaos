fb5f9413981900c4a35ced5d6dd831cffd4524eb
muhammedkan <muhammedkan@gmail.com>
2020-12-19 21:22:36 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewYearChaos
+{
+    class Program
+    {
+        static void minimumBribes(int[] q)
+        {
+            int bribe = 0;
+            bool chaotic = false;
+            int n = q.Length;
+            for (int i = 0; i < n; i++)
+            {
+                if (q[i] - (i + 1) > 2)
+                {
+                    chaotic = true;
+                    break;
+                }
+                for (int j = Math.Max(0, q[i] - 2); j < i; j++)
+                    if (q[j] > q[i])
+                        bribe++;
+            }
+            if (chaotic)
+                Console.WriteLine("Too chaotic");
+            else
+                Console.WriteLine(bribe);
+        }
+
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                int[] q = Array.ConvertAll(Console.ReadLine().Split(' '), qTemp => Convert.ToInt32(qTemp))
+                ;
+                minimumBribes(q);
+            }
+        }
+    }
+}
